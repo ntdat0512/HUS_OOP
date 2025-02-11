35c4cc46bdc3d7c07068e98492ab9991955904d6
Nguyễn Tiến Đạt <ngtiendat0512@gmail.com>
2025-02-11 17:33:15 +0700

    Update Factorial.java

--- Lab1/Debugging, Tracing Programs using a Graphic De- bugger/Factorial.java
+++ Lab1/Debugging, Tracing Programs using a Graphic De- bugger/Factorial.java
@@ -1,1 +1,12 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 20;
+        long factorial = 1;
 
+//        n! = 1*2*3*...*n
+        for(int i=1; i<=n; i++){
+            factorial = factorial * i;
+        }
+        System.out.println("The factorial of" + n + " is " + factorial);
+    }
+}
